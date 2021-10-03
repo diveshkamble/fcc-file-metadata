ffa6908f97f54391793f63be24c5ced1879f78eb
Divesh Kamble <diveshkamble@gmail.com>
2021-10-03 20:45:53 +0530

    Test Commit

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
 
 var app = express();
 
+app.use(fileUpload());
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
-
+app.post("/api/fileanalyse", async (req, res) => {
+  //console.log(req.files);
+  fileName = await req.files.upfile.name;
+  //console.log(fileName);
+  fileType = await req.files.upfile.mimetype;
+  //console.log(fileType);
+  fileSize = await req.files.upfile.size;
+  //console.log(fileSize);
+  res.send({
+    name: fileName,
+    type: fileType,
+    size: fileSize,
+  });
+});
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
